f1c89307eed48f4735a144ba4058e957225f4315
Farheen2912-india <farheenqspider@gmail.com>
2026-07-14 17:01:02 +0530

    Refactor FlightSearchPage to inherit from BasePage

--- pages/flight_search_page.py
+++ pages/flight_search_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pages.flight_search_page import FlightSearchPage
+from pages.base_page import BasePage
 
 
 class FlightSearchPage(BasePage):
