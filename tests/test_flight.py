4f39a7cdecf972c068bd609d991df6f293c913ff
Farheen2912-india <farheenqspider@gmail.com>
2026-07-14 16:59:25 +0530

    Update import paths in test_flight.

--- tests/test_flight.py
+++ tests/test_flight.py
@@ -1,6 +1,6 @@
 import pytest
-from Ixigo_Login_Framework.pages.flight_search_page import FlightSearchPage
-from Ixigo_Login_Framework.utilities.read_excel import get_flight_data
+from pages.flight_search_page import FlightSearchPage
+from utilities.read_excel import get_flight_data
 
 
 @pytest.mark.parametrize("from_city,to_city",get_flight_data())
